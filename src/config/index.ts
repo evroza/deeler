d07a87bc9e4c265f0e7115c75cbbd5e51a198d46
Evans R <evansrutoh@gmail.com>
2021-12-04 01:37:56 +0300

    Cleanup and add Readme

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,29 +8,7 @@ const config = convict({
     format: Number,
     default: 3001,
     env: 'APP_PORT',
-  },
-  mysql: {
-    host: {
-      doc: 'The MYSQL HOST',
-      default: '127.0.0.1',
-      env: 'DATABASE_HOST',
-    },
-    user: {
-      doc: 'The MYSQL user',
-      default: 'root',
-      env: 'DATABASE_USER',
-    },
-    pass: {
-      doc: 'The Mysql Password',
-      default: 'qwerty123',
-      env: 'DATABASE_PASS',
-    },
-    schema: {
-      doc: 'The MYSQL Schema',
-      default: 'deel_db',
-      env: 'DATABASE_SCHEMA',
-    },
-  },
+  }
 });
 
 export default config;
